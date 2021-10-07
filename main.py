70fa21b258e3c2fed13d9bb5cacc48f43056255a
Dillon callahan <dcal2017@gmail.com>
2021-10-07 18:27:22 -0400

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from part1 import setCreator, CreateTrainSet, CreateChallengeSet
+from part2 import trainer, testing, weightHeatMap, testChallengeSet
+from problem2 import NewTrainer, Newtesting, NewweightHeatMap, NewtestChallengeSet
+
+#setCreator()
+#CreateChallengeSet()
+#CreateTrainSet()
+
+#trainer()
+#testing()
+#weightHeatMap()
+#testChallengeSet()
+
+#NewTrainer()
+#Newtesting()
+#NewweightHeatMap()
+NewtestChallengeSet()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
